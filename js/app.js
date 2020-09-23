93b0e49f0a84f30111b51829ff046c8e46dae59c
Louisc10 <42336657+Louisc10@users.noreply.github.com>
2020-09-23 21:23:02 +0700

    a little update to initialize and add some docs

--- js/app.js
+++ js/app.js
@@ -1,8 +1,46 @@
 import * as THREE from '../three.js/build/three.module.js';
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var renderer, camera, scene;
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+var init = function() {
+    renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+    renderer.autoClear = false;
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    scene = new THREE.Scene();
+
+    const fov = 45;
+    camera = new THREE.PerspectiveCamera(fov, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.copy(new THREE.Vector3(60, 40, 60));
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+}
+
+var update = function() {
+
+}
+
+var render = function() {
+    renderer.clear();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.render(scene, camera);
+};
+
+var gameLoop = function() {
+    // requestAnimationFrame(gameLoop);
+    update();
+    render();
+};
+
+window.addEventListener('resize', () => {
+    render();
+}, false);
+
+init();
+gameLoop();
