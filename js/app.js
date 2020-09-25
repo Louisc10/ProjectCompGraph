42d080a52b496e7cca8b602e824298d5c190640a
Louisc10 <42336657+Louisc10@users.noreply.github.com>
2020-09-25 15:56:41 +0700

    Add some Moon Light, and 2 cam

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 import * as THREE from '../three.js/build/three.module.js';
+// import * as Asphalt from './Asphalt';
 
-var renderer, camera, scene;
+var renderer, scene;
+var thirdPersonCamera, carDriverCamera;
+var moonlight;
+
+var asphalt;
 
 var init = function() {
     renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
@@ -11,12 +16,26 @@ var init = function() {
     document.body.appendChild(renderer.domElement);
 
     scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x5555ff);
 
     const fov = 45;
-    camera = new THREE.PerspectiveCamera(fov, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.copy(new THREE.Vector3(60, 40, 60));
-    camera.lookAt(new THREE.Vector3(0, 0, 0));
+    thirdPersonCamera = new THREE.PerspectiveCamera(fov, window.innerWidth / window.innerHeight, 0.1, 1000);
+    thirdPersonCamera.position.copy(new THREE.Vector3(60, 40, 60));
+    thirdPersonCamera.lookAt(new THREE.Vector3(0, 0, 0));
 
+    carDriverCamera = new THREE.PerspectiveCamera(fov, window.innerWidth / window.innerHeight, 0.1, 1000);
+    carDriverCamera.position.copy(new THREE.Vector3(60, 40, 60));
+    carDriverCamera.lookAt(new THREE.Vector3(0, 0, 0));
+
+    moonlight = new THREE.PointLight({
+        color: "#F4F1C9",
+        distance: 1000,
+        decay: 1.5
+    });
+    moonlight.position.copy(new THREE.Vector3(0, 500, 250));
+
+    // asphalt = new Asphalt();
+    // scene.add(asphalt);
 }
 
 var update = function() {
@@ -26,14 +45,14 @@ var update = function() {
 var render = function() {
     renderer.clear();
     renderer.setSize(window.innerWidth, window.innerHeight);
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
+    thirdPersonCamera.aspect = window.innerWidth / window.innerHeight;
+    thirdPersonCamera.updateProjectionMatrix();
 
-    renderer.render(scene, camera);
+    renderer.render(scene, thirdPersonCamera);
 };
 
 var gameLoop = function() {
-    // requestAnimationFrame(gameLoop);
+    requestAnimationFrame(gameLoop);
     update();
     render();
 };
